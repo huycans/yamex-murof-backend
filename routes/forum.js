99e822033bf052293578a6e457ca82ff218b9528
huycans <huycan_lk@yahoo.com.vn>
2019-06-24 16:14:55 +0700

    Basic non-auth forum router

--- routes/forum.js
+++ routes/forum.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const model = require("../models/");
+const Forums = require("../models/forum");
 // const authenticate = require("../authenticate");
 // const cors = require("./cors");
 
@@ -10,15 +10,43 @@ forumRouter.use(bodyParser.json());
 forumRouter
 .route("/")
 .put((req, res, next) => {
-  console.log("Create a new forum");
-  res.end("Create a new forum");
+  // TODO: implement admin auth check here
+  Forums.create({
+    name: req.body.name,
+    coverUrl: req.body.coverUrl,
+    bikeInfo: req.body.bikeInfo,
+    path: String(req.body.name).replace(/ /g,'').toLowerCase(),
+    moderators: req.body.moderators ? req.body.moderators : null 
+  })
+  .then(forum => {
+    console.log("Forum created: ", forum.name);
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json(forum);
+  },
+  err => {
+    next(err);
+  })
+  .catch(err => {
+    next(err);
+  });
 });
 
 forumRouter
 .route("/all")
 .get((req, res, next) => {
-  console.log("Returning all forums");
-  res.end("Returning all forums");
+  Forums.find()
+  .then(forum => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json(forum);
+  },
+  err => {
+    next(err);
+  })
+  .catch(err => {
+    next(err);
+  });
 });
 
 module.exports = forumRouter;
