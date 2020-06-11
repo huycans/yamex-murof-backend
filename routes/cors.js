2b83ca7977b68e2fa95dcd40f5723b026c899f2a
huycans <huycan_lk@yahoo.com.vn>
2020-06-11 16:03:15 -0400

    Change cors whitelist

--- routes/cors.js
+++ routes/cors.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const whitelist = ["http://localhost:3000","http://localhost:3001", "https://localhost:3443", "http://Huy:3001"];
+const whitelist = ["http://localhost:3000","http://localhost:3001", "https://localhost:3443", "https://yamex.herokuapp.com/"];
 var corsOptionDelegate = (req, callback) => {
   var corsOptions;
   if (whitelist.indexOf(req.header("Origin")) !== -1) {
