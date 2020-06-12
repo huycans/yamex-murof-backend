8847970a4fbd252e476b757615301e651b845855
huycans <huycan_lk@yahoo.com.vn>
2020-06-12 16:21:19 -0400

    Change paths in app.js

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ app.use(cookieParser());
 
 app.use(passport.initialize());
 
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.use(express.static(path.resolve(__dirname, './frontend/build')));
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  response.sendFile(path.resolve(__dirname, './frontend/build', 'index.html'));
 });
 
 
