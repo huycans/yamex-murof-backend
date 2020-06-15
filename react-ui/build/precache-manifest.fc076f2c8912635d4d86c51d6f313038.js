b2b48f5c1c9a37361e418619166370b4fd8784e6
huycans <huycan_lk@yahoo.com.vn>
2020-06-15 11:32:00 -0400

    Retry without RAC buildpack+hardcode server url

---
+++ react-ui/build/precache-manifest.fc076f2c8912635d4d86c51d6f313038.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d992c14112a97ae99c0c2fff61eeb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5007c665400c5faf1106",
+    "url": "/static/css/2.8cabcd47.chunk.css"
+  },
+  {
+    "revision": "bdad3e40281846275bbd",
+    "url": "/static/css/main.71502d04.chunk.css"
+  },
+  {
+    "revision": "5007c665400c5faf1106",
+    "url": "/static/js/2.95d597bd.chunk.js"
+  },
+  {
+    "revision": "bdad3e40281846275bbd",
+    "url": "/static/js/main.e5202b15.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5c14513606b968c5a7c81941532f69a5",
+    "url": "/static/media/logo.5c145136.png"
+  }
+]);
