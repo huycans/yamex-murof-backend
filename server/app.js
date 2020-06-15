bb96448e848938ec5bda0ddbbe049274d4276f05
huycans <huycan_lk@yahoo.com.vn>
2020-06-15 11:14:05 -0400

    Fix path typo

--- server/app.js
+++ server/app.js
@@ -28,9 +28,9 @@ app.use(cookieParser());
 
 app.use(passport.initialize());
 
-app.use(express.static(path.resolve(__dirname, './react-ui/build')));
+app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 app.get('/', function(req, res) {
-  res.sendFile(path.resolve(__dirname, './react-ui/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
 });
 
 
